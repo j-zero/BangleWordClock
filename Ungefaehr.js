a9ede0fb642cf257d13f3dfc666069e3500bb462
j-zero <er@j-zero.de>
2022-03-23 15:35:58 +0100

    widgets

--- Ungefaehr.js
+++ Ungefaehr.js
@@ -1,3 +1,42 @@
+//https://github.com/rozek/banglejs-2-widgets-on-background
+(function () {
+  Bangle.drawWidgets = function () {
+    var w = g.getWidth(), h = g.getHeight();
+
+    var pos = {
+      tl:{x:0,   y:0,    r:0, c:0}, // if r==1, we're right->left
+      tr:{x:w-1, y:0,    r:1, c:0},
+      bl:{x:0,   y:h-24, r:0, c:0},
+      br:{x:w-1, y:h-24, r:1, c:0}
+    };
+
+    if (global.WIDGETS) {
+      for (var wd of WIDGETS) {
+        var p = pos[wd.area];
+        if (!p) continue;
+
+        wd.x = p.x - p.r*wd.width;
+        wd.y = p.y;
+
+        p.x += wd.width*(1-2*p.r);
+        p.c++;
+      }
+
+      g.reset();                                 // also loads the current theme
+
+      try {
+        for (var wd of WIDGETS) {
+          g.setClipRect(wd.x,wd.y, wd.x+wd.width-1,23);
+          wd.draw(wd);
+        }
+      } catch (e) { print(e); }
+
+      g.reset();                               // clears the clipping rectangle!
+    }
+  };
+})();
+
+
 const locale = require("locale");
 
 
@@ -318,7 +357,7 @@ function draw() {
   queueDraw();
 }
 
-//Bangle.loadWidgets();
+//
 
 // Stop updates when LCD is off, restart when on
 Bangle.on('lcdPower',on=>{
@@ -340,8 +379,8 @@ Bangle.setUI("clock");
 // clean app screen
 g.clear();
 
-
-//Bangle.drawWidgets();
+Bangle.loadWidgets();
+Bangle.drawWidgets();
 
 // draw now
 draw();
